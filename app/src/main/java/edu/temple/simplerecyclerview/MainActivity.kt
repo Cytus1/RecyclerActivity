baf6f4c82ec7c7debf589ac142b5ab630fd2da39
Cytus1 <xyloaex@gmail.com>
2026-02-10 12:20:05 -0500

    recycler view

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,19 +2,30 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.SimpleAdapter
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import androidx.recyclerview.widget.LinearLayoutManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO Step 1: Reference RecyclerView object
+        // Step 1: Reference RecyclerView object
+
+        //https://developer.android.com/develop/ui/views/layout/declaring-layout
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        //TODO Step 2: Provide a LayoutManager
+        // Step 2: Provide a LayoutManager
+
+        // https://developer.android.com/develop/ui/views/layout/recyclerview
+
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
+        //recyclerView.layoutManager = LinearLayoutManager(this)
 
 
-        //TODO Step 4: Provide a RecyclerView.Adapter
+        // Step 4: Provide a RecyclerView.Adapter
+        recyclerView.adapter = NumberDisplayAdapter(arrayOf(1, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10))
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -2,13 +2,54 @@ package edu.temple.simplerecyclerview
 
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+
+
+class NumberDisplayAdapter (private val numbers: Array<Int>)
+
+/* Step 3a: Provide Adapter Parent */ : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
     class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
-    // TODO Step 3b: Complete function definitions for adapter
+    //  Step 3b: Complete function definitions for adapter
+
+    // https://developer.android.com/develop/ui/views/layout/recyclerview
+
+    //professor's code
+//    override fun OncreateViewHolder(
+//        parent: ViewGroup,
+//        viewType: Int
+//    ) = NumberViewHolder (
+//        TextView(parent.context).apply{
+//
+//        }
+//    )
+//
+//    override fun onBindVIewHolder(
+//        holder: NumberViewHolder,
+//        position: Int
+//    ){
+//        holder.textView.text = ((position + 1) * 5).toString()
+//    }
+//
+//    override fun getItemCount(): Int{
+//
+//    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(TextView(viewGroup.context))
+    }
+
+    //override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+        holder.textView.text = numbers[position].toString()
+    }
+
+    override fun getItemCount(): Int = numbers.size
+
 
 }
